d0eebf6d8eac17de5d75d4c07a372ee63a9e3dab
Sara <saragorleta@yahoo.com>
2021-02-16 14:02:02 +0100

    aggiunto js vue per header e footer

---
+++ js/script.js
@@ -0,0 +1,48 @@
+var app = new Vue({
+  el:'#app',
+  data:{
+    navBar:[
+      {
+        nome:'Home'
+      },
+      {
+        nome:'About'
+      },
+      {
+        nome:'Department'
+      },
+      {
+        nome:'Articles'
+      },
+    ],
+    footerContacts:[
+      {
+        indirizzo:'121 King Street, London United Kingdom',
+        telefono:'1.800.458.556'
+      }
+    ],
+    footerDepartments:[
+      {
+        'Cardiology', 'Pediatrics', 'Diabetes-Care','Pre-natel-Care','Ultrasoudechocardiohram'
+      }
+    ],
+    indexNavSelezionato:0,
+    indexContactsSelezionato:0,
+    indexDepartmentsSelezionato:0
+
+  },
+
+  methods:{
+    aggiungiNav(index){
+      this.indexNavSelezionato=index;
+      console.log(index);
+    },
+    aggiungiContacts(index){
+      this.indexContactsSelezionato=index;
+    },
+    aggiungiDepartments(index){
+      this.indexDepartmentsSelezionato=index;
+    }
+  }
+
+});
